dab5073de2cf0eea8b48fa4be009704fed2fe5ea
janfalkin <jan.falkin@eluv.io>
2021-10-11 21:06:33 +0000

    cleanup, better logging in jpc

--- src/lib.rs
+++ src/lib.rs
@@ -22,65 +22,6 @@ lazy_static! {
   static ref CALLMAP: Mutex<HashMap<String, HandlerFunction>> = Mutex::new(HashMap::new());
 }
 
-/*
-{
-  "id": "b31b79ed-a0fd-4267-9344-e24a3fb463d4",
-  "jpc": "1.0",
-  "method": "content",
-  "params": {
-    "http": {
-      "headers": {
-        "Content-type": ["application/json"]
-      },
-      "path": "/proxy",
-      "query": {
-        "API_KEY": "AIzaSyCppaD53DdPEetzJugaHc2wW57hG0Y5YWE",
-        "CONTEXT": "012842113009817296384:qjezbmwk0cx",
-        "QUERY": "fabric"
-      },
-      "verb": "unused"
-    }
-  },
-  "QInfo": {
-    "hash": "hq__3B4dcp2T2gqYLYPukxMYWsVkQBH5DuA8WxjGd7dcXodzZf15F3VFNd96Zc1B9QszZmKAH",
-    "id": "iq__AsQaUWfFcSJFYNyeyVMi6V",
-    "qlib_id": "ilibBKAMfarVtkNhJiW8Xou2VW",
-    "type": "",
-    "write_token": "tqw__K2gHQyYdeMXKq2hTfZmNwPaiaXoEmJYiY6NeyvenGK8pJ6htkG2ggbwe8GjKUu3fC1ZANT3v"
-  }
-}
-
-{
-  "id": "4a051bc6-35d9-4ee9-9230-9c787e5e2061",
-  "jpc": "1.0",
-    "http": {
-      "verb": "GET",
-      "path": "/image/default/assets/birds.jpg",
-      "fragment": "",
-      "query": {
-        "height": [
-          "666"
-        ]
-      },
-      "headers": null,
-      "host": "http://localhost:8008/",
-      "content_length": 0,
-      "client_ip": "",
-      "self_url": "http://localhost:8008/",
-      "proto": ""
-    }
-  },
-  "qinfo": {
-    "id": "iq__Hip8Cg3wuni31yzWsRZYzJ",
-    "hash": "hq__3Nq84nb7koGj8wf1xqJVg1WQBkz5nFgGoZt46Hc8gH8yyz3WsRrQ5gLJH9VMm8pDPJu8i",
-    "write_token": "tqw__K2jJSBHiHqASXaqy4d3YiyBnAdfTLFR9dJUqtSyS8CiXC9FsBprRfMPFKadrNwXPXvjjf1AK",
-    "type": "",
-    "qlib_id": "ilibKjDXjtfLWt3msxUZQA2JRm"
-  }
-}
-
-*/
-
 #[derive(Debug)]
 struct ElvError {
     details: String
@@ -194,11 +135,11 @@ impl<'a> BitcodeContext<'a> {
       actual_len = len
     }
     let v = serde_json::json!(src[..actual_len]);
-    return guest::host_call(&id, &stream, &"Write".to_string(), &serde_json::to_vec(&v).unwrap());
+    return host_call(&id, &stream, &"Write".to_string(), &serde_json::to_vec(&v).unwrap());
   }
 
   pub fn write_stream_auto(id:String, stream:String,  src:&'a [u8]) -> CallResult {
-    return guest::host_call(&id, &stream, &"Write".to_string(), src);
+    return host_call(&id, &stream, &"Write".to_string(), src);
   }
 
   pub fn callback(&'a self, status:usize, content_type:&str, size:usize) -> CallResult{
@@ -218,7 +159,7 @@ impl<'a> BitcodeContext<'a> {
 
   pub fn read_stream(&'a mut self, stream_to_read:String, sz:usize) -> CallResult {
         let input = serde_json::json![sz];
-        return guest::host_call(self.request.id.as_str(),stream_to_read.as_str(), &"Read", &serde_json::to_vec(&input).unwrap());
+        return host_call(self.request.id.as_str(),stream_to_read.as_str(), &"Read", &serde_json::to_vec(&input).unwrap());
   }
 
   pub fn temp_dir(&'a mut self) -> String {
@@ -276,7 +217,7 @@ impl<'a> BitcodeContext<'a> {
   }
 
   fn call(&'a mut self, ns: &str, op: &str, msg: &[u8]) -> CallResult{
-    return guest::host_call(self.request.id.as_str(),ns,op,msg);
+    return host_call(self.request.id.as_str(),ns,op,msg);
   }
 
   pub fn call_function(&'a self, fn_name:&str , params:serde_json::Value, module:&str) -> CallResult {
@@ -294,7 +235,7 @@ impl<'a> BitcodeContext<'a> {
     console_log("CALL STRING");
     console_log(call_str);
     console_log(&call_str2);
-    return guest::host_call(self.request.id.as_str(),module, fn_name, &call_val);
+    return host_call(self.request.id.as_str(),module, fn_name, &call_val);
   }
     // NewStream creates a new stream and returns its ID.
     pub fn new_stream(&'a self) -> String {
@@ -377,9 +318,6 @@ impl<'a> BitcodeContext<'a> {
 
 }
 
-
-
-
 #[no_mangle]
 pub fn register_handler(name: &str, h: HandlerFunction) {
   CALLMAP.lock().unwrap().insert(name.to_string(), h);
@@ -387,18 +325,13 @@ pub fn register_handler(name: &str, h: HandlerFunction) {
 
 #[no_mangle]
 pub fn jpc<'a>(_msg: &'a [u8]) -> CallResult {
-  guest::console_log(&"Hello");
+  console_log(&"In jpc");
   let input_string = str::from_utf8(_msg)?;
-  guest::console_log(&"Hello Again");
-  guest::console_log(&input_string);
-
+  console_log(&format!("parameters = {}", input_string));
   let json_params: Request = serde_json::from_str(input_string)?;
-  guest::console_log(&"Hello Again");
-
+  console_log(&"Request parsed");
   let mut bcc = BitcodeContext::new(&json_params);
-  let j = serde_json::to_string(&json_params)?;
-  guest::console_log(&"Hello Again");
-  guest::console_log(&j);
+  console_log(&"Parameters parsed");
   let split_path: Vec<&str> = bcc.request.params.http.path.as_str().split('/').collect();
   console_log(&format!("splitpath={:?}", split_path));
   match CALLMAP.lock().unwrap().get(split_path[1]) {
