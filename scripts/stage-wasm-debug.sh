961fe155e37e2f22cc31e6bf886560ec47479e80
janfalkin <jan.falkin@eluv.io>
2022-11-23 12:41:38 -0800

    no wasm-gc it removed symbols for debug

--- scripts/stage-wasm-debug.sh
+++ scripts/stage-wasm-debug.sh
@@ -12,12 +12,13 @@ if [ $# -ne 2 ]
 then
     echo "Bad number of args"
 else
-    wasm-gc $1/target/wasm32-unknown-unknown/debug/rproxy.wasm $2/bitcode/wasm/rproxy/rproxy.wasm
-    wasm-gc $1/target/wasm32-unknown-unknown/debug/image.wasm $2/bitcode/wasm/image/image.wasm
-    wasm-gc $1/target/wasm32-unknown-unknown/debug/real_img.wasm $2/bitcode/wasm/image/real_img.wasm
-    wasm-gc $1/target/wasm32-unknown-unknown/debug/search.wasm $2/bitcode/wasm/search/search.wasm
-    wasm-gc $1/target/wasm32-unknown-unknown/debug/everything.wasm $2/bitcode/wasm/everything/everything.wasm
-    wasm-gc $1/samples/library/library.wasm $2/bitcode/wasm/library/library.wasm
-    wasm-gc $1/samples/proxy/proxy.wasm $2/bitcode/wasm/proxy/proxy.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/rproxy.wasm $2/bitcode/wasm/rproxy/rproxy.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/image.wasm $2/bitcode/wasm/image/image.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/real_img.wasm $2/bitcode/wasm/image/real_img.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/search.wasm $2/bitcode/wasm/search/search.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/everything.wasm $2/bitcode/wasm/everything/everything.wasm
+    cp $1/target/wasm32-unknown-unknown/debug/objtar.wasm $2/bitcode/wasm/objtar/objtar.wasm
+    cp $1/samples/library/library.wasm $2/bitcode/wasm/library/library.wasm
+    cp $1/samples/proxy/proxy.wasm $2/bitcode/wasm/proxy/proxy.wasm
 fi
 
