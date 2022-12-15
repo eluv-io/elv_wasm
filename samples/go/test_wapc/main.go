838e0425245811aaad942780570df57cc08418f9
jan falkin <jan.falkin@eluv.io>
2022-12-14 18:50:22 -0800

    remove logs from test

--- samples/go/test_wapc/main.go
+++ samples/go/test_wapc/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 
 	wapc "github.com/wapc/wapc-guest-tinygo"
@@ -21,18 +20,14 @@ func echo(bi []byte) ([]byte, error) {
 func factorial(bi []byte) ([]byte, error) {
 	factVal := uint64(1)
 	sin := string(bi)
-	wapc.ConsoleLog(fmt.Sprintf("input = %s", sin))
 
 	n, err := strconv.Atoi(sin)
 	if err != nil {
 		return nil, err
 	}
-	wapc.ConsoleLog(fmt.Sprintf("Atoi = %d %d", n, uint64(n)))
 	for i := uint64(1); i <= uint64(n); i++ {
-		factVal *= i // mismatched types int64 and int
+		factVal *= i
 	}
-	wapc.ConsoleLog(fmt.Sprintf("fact = %d", factVal))
 	s1 := strconv.FormatUint(factVal, 10)
-	wapc.ConsoleLog(fmt.Sprintf("s = %s", s1))
 	return []byte(s1), nil
 }
