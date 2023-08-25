adfdf5835ba78737390be9a80f084654c0dd0233
Jan Falkin <jan.falkin@eluv.io>
2023-08-25 15:05:21 -0700

    fix mac and linux

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-use std::process::Command;
 use std::env;
+use std::process::Command;
 
 pub fn execute(exe: &str, args: &[&str]) {
     Command::new(exe)
@@ -8,13 +8,18 @@ pub fn execute(exe: &str, args: &[&str]) {
         .unwrap_or_else(|_| panic!("failed to start external executable {exe}"));
 }
 
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+}
+
+#[cfg(target_arch = "wasm32")]
 fn main() {
     println!("handling building assemblyscript");
     let mut pbase = env::current_dir().unwrap();
     pbase.push("samples");
     env::set_current_dir(pbase.clone()).unwrap();
     let do_asc = env::var_os("BUILD_ASC").is_some();
-    if do_asc{
+    if do_asc {
         execute("npm", &["install", "asc"]);
         execute("npm", &["run", "asbuild"]);
     }
@@ -22,6 +27,24 @@ fn main() {
     pbase.push("test_wapc");
 
     env::set_current_dir(pbase).unwrap();
-    execute("tinygo", &["build", "-o", "test_wapc.wasm", "-target=wasi", "-no-debug", "main.go"]);
-    execute("mv", &["-f", "-u", "test_wapc.wasm", "../../../target/wasm32-unknown-unknown/release/"])
+    execute(
+        "tinygo",
+        &[
+            "build",
+            "-o",
+            "test_wapc.wasm",
+            "-target=wasi",
+            "-no-debug",
+            "main.go",
+        ],
+    );
+    execute(
+        "mv",
+        &[
+            "-f",
+            "-u",
+            "test_wapc.wasm",
+            "../../../target/wasm32-unknown-unknown/release/",
+        ],
+    )
 }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#[cfg(not(target_arch = "wasm32"))]
+extern crate wapc;
+
+fn main() {
+    // Your code here
+}
